98fa2c1d87b593c69f6cef80d785dc2f75c2b83a
dave griffiths <dave@fo.am>
2018-04-18 13:33:42 +0000

    remote receptor changes

--- web/server.scm
+++ web/server.scm
@@ -223,7 +223,7 @@
  start
  ;; port number is read from command line as argument
  ;; ie: ./server.scm 8080
- #:listen-ip "127.0.0.1"
+ #:listen-ip "10.42.0.1"
  #:port (string->number (command-line #:args (port) port))
  #:command-line? #t
  #:servlet-path "/game"
